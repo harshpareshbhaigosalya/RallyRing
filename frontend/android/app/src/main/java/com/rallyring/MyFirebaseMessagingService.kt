94b06f35b8801b0a501fcaffd40387764109806b
harshpareshbhaigosalya <harshgosaliya9@gmail.com>
2026-03-29 18:50:11 +0530

    Added custom MyFirebaseMessagingService wrapper to bypass Chinese OEM battery killer logic on Android

---
+++ frontend/android/app/src/main/java/com/rallyring/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.rallyring
+
+import com.google.firebase.messaging.RemoteMessage
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingService
+
+class MyFirebaseMessagingService : ReactNativeFirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
